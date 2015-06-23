d28cf3a53904a657ed59fe43ef07be14a7041571
frenz <giuseppe@gtgames.it>
2015-06-23 11:06:19 +0200

    added Cluster interfaces

---
+++ assignment3/Leaf.java
@@ -0,0 +1,29 @@
+package assignment3;
+
+public class Leaf implements Cluster {
+
+	@Override
+	public int getDepth() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getWidth() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public UnitRow getUnits() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean hasChildren() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
